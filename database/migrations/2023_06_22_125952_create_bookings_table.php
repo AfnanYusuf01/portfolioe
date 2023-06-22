8f4982378666732686240331cd3a6085253b7693
AfnanYusuf01 <134181508+AfnanYusuf01@users.noreply.github.com>
2023-06-23 00:47:09 +0700

    Update 2023_06_22_125952_create_bookings_table.php
    lupa migrasinya

--- database/migrations/2023_06_22_125952_create_bookings_table.php
+++ database/migrations/2023_06_22_125952_create_bookings_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
+            $table->string('email');
+            $table->string('subjek');
+            $table->string('pesan');
             $table->timestamps();
         });
     }
